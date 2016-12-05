08f09118407aee8d3f374cd88124d314c3cd61a0
Eugene Sia <eugene@eugenesia.co.uk>
2016-12-05 00:31:59 +0000

    Don't spawn enemy right in front of player

--- Assets/Project/Scripts/EnemyManager.cs
+++ Assets/Project/Scripts/EnemyManager.cs
@@ -27,10 +27,19 @@ public class EnemyManager : MonoBehaviour {
 		GameObject newEnemy = Instantiate(ninjaPrefab);
 
 		// Spawn enemy randomly along x-axis on the platform.
-		float xpos = Random.Range(-2.5f, 2.5f);
+		float xpos = Random.Range(-5f, 5f);
+
+		// If enemy is right in front of player (-1.5 < X < +1.5), it will
+		// collide with player, so move it to the side.
+		if (xpos > -1.5f && xpos < 0f) {
+			xpos = -1.5f;
+		}
+		else if (xpos > 0f && xpos < 1.5f) {
+			xpos = 1.5f;
+		}
 
 		// Start off at the end of the platform, and move towards player.
-		newEnemy.transform.position = new Vector3(xpos, 0f, 120f);
+		newEnemy.transform.position = new Vector3(xpos, 0f, 90f);
 
 		// Cause enemies to go into "Run" state so they run towards player.
 		Animator anim = newEnemy.GetComponent<Animator>();
