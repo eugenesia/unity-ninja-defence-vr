cf9b3f3f521229f75615719e4a6d09c18e1bfee2
Eugene Sia <eugene@eugenesia.co.uk>
2016-12-05 02:59:01 +0000

    Destroy everything going out of game boundaries

---
+++ Assets/Project/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// Destroy any object out of the game boundaries.
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class DestroyOutOfBounds : MonoBehaviour {
+
+	// Destroy everything that leaves the game boundaries.
+	void OnTriggerExit(Collider other) {
+		Destroy(other.gameObject);
+	}
+}
