4d71f149153c62281157fd57a4feddf474704b54
Eugene Sia <eugene@eugenesia.co.uk>
2016-12-05 02:28:23 +0000

    Blink enemy before dying

--- Assets/Project/Scripts/Enemy.cs
+++ Assets/Project/Scripts/Enemy.cs
@@ -7,6 +7,12 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
+	// Before the enemy dies, it blinks.
+	// Number of blinks.
+	private int blinkCount = 3;
+	// Interval between blinks.
+	private float blinkInterval = 0.2f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -20,9 +26,26 @@ public class Enemy : MonoBehaviour {
 	// Check if we collided with a player projectile, if so it's dead.
 	void OnCollisionEnter(Collision collision) {
 		if (collision.gameObject.CompareTag("Fire")) {
-
-			// Deactivate this enemy as it's dead.
-			gameObject.SetActive(false);
+			StartCoroutine("Die");
 		}
+	}
+
+	// Start dying sequence.
+	IEnumerator Die() {
+
+		// Stop running animation and make it idle.
+		Animator anim = GetComponent<Animator>();
+		anim.SetBool("Moving", false);
+		anim.SetBool("Running", false);
+
+		// Blink by disabling and enabling the renderer.
+		Renderer renderer = GetComponentInChildren<Renderer>();
+		for (int i = 0; i < blinkCount * 2; i++) {
+			renderer.enabled = ! renderer.enabled;
+			yield return new WaitForSeconds(blinkInterval);
+		}
+
+		// Destroy this enemy as it's dead.
+		Destroy(gameObject);
 	}
 }
---
+++ Assets/Project/Scripts/Shuriken.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// Manage individual shuriken behavior.
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class Shuriken : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Disappear on hitting enemy.
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.CompareTag("Enemy")) {
+			Destroy(gameObject);
+		}
+	}
+}
