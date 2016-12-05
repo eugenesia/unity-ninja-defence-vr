dab42e451d113335a6036a5934739b5a885f725a
Eugene Sia <eugene@eugenesia.co.uk>
2016-12-05 01:53:15 +0000

    Deactivate enemy on projectile hit

---
+++ Assets/Project/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿/// <summary>
+/// Manage individual enemy behavior.
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Check if we collided with a player projectile, if so it's dead.
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.CompareTag("Fire")) {
+
+			// Deactivate this enemy as it's dead.
+			gameObject.SetActive(false);
+		}
+	}
+}
